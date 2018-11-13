d7740e69504b1f5764b0f0d42262ca5227b79ab4
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-11-13 03:15:06 +0100

    lint.sh: refactor gometalinter install

--- lint.sh
+++ lint.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-export PATH=$HOME/go/bin:$PATH
+export PATH="$(pwd)/bin:$PATH"
 
-hasExecutable=$(which gometalinter 2>/dev/null)
-if [ "$hasExecutable" == "" ]; then
-  curl -L https://git.io/vp6lP | sh
-  mv ./bin/* "$FOME/go/bin/"
-  pushd $HOME
-    go get github.com/davecgh/go-spew/spew
-    go get github.com/pmezard/go-difflib/difflib
-    go get github.com/stretchr/testify/assert
-    go get golang.org/x/sys/unix
-  popd
-fi
+curl -L https://git.io/vp6lP | sh
+pushd $HOME
+  go get -u github.com/davecgh/go-spew/spew
+  go get -u github.com/pmezard/go-difflib/difflib
+  go get -u github.com/stretchr/testify/assert
+  go get -u golang.org/x/sys/unix
+popd
 
 gometalinter ./...

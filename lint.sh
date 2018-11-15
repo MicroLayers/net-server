8fa92916abea69b3e5d76aa7f2c99d77f52bec02
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-11-16 00:46:30 +0100

    Fix & tune drone pipeline

--- lint.sh
+++ lint.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
-export PATH="$(pwd)/bin:$PATH"
+set -o xtrace
 
+export BINDIR=$HOME/go/bin
+export PATH="${BINDIR}:$PATH"
+echo "Installing gometalinter..."
 curl -L https://git.io/vp6lP | sh
-pushd $HOME
+
+pushd $HOME/go
+  echo "Installing gometalinter deps..."
   go get -u github.com/davecgh/go-spew/spew
   go get -u github.com/pmezard/go-difflib/difflib
   go get -u github.com/stretchr/testify/assert
   go get -u golang.org/x/sys/unix
 popd
 
-gometalinter ./...
+echo "Executing gometalinter..."
+gometalinter --fast --vendor ./...
